0527d4a5c8ab3ded7d637e151045cedae25e11a2
zunigamatias <oliveirazmatias@gmail.com>
2025-10-08 20:00:07 -0300

    set up the environment

---
+++ ra2_main.cpp
@@ -0,0 +1,14 @@
+
+
+int main(int argc, char const *argv[])
+{
+    auto backend = []() {
+        while (true)
+        {
+            /* code */
+        }
+        
+    };
+    backend();
+    return 0;
+}
