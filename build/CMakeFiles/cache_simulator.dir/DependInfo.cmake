708bb611305c2913cad3e9742df15471d11b4507
zunigamatias <oliveirazmatias@gmail.com>
2025-10-13 00:43:53 -0300

    cmake file created

---
+++ build/CMakeFiles/cache_simulator.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tiasz/cache_algorithms/algorithms/cache.cpp" "CMakeFiles/cache_simulator.dir/algorithms/cache.cpp.o" "gcc" "CMakeFiles/cache_simulator.dir/algorithms/cache.cpp.o.d"
+  "/home/tiasz/cache_algorithms/algorithms/lfu/lfu.cpp" "CMakeFiles/cache_simulator.dir/algorithms/lfu/lfu.cpp.o" "gcc" "CMakeFiles/cache_simulator.dir/algorithms/lfu/lfu.cpp.o.d"
+  "/home/tiasz/cache_algorithms/algorithms/random_replacement/randomReplacement.cpp" "CMakeFiles/cache_simulator.dir/algorithms/random_replacement/randomReplacement.cpp.o" "gcc" "CMakeFiles/cache_simulator.dir/algorithms/random_replacement/randomReplacement.cpp.o.d"
+  "/home/tiasz/cache_algorithms/algorithms/s3_fifo/s3Fifo.cpp" "CMakeFiles/cache_simulator.dir/algorithms/s3_fifo/s3Fifo.cpp.o" "gcc" "CMakeFiles/cache_simulator.dir/algorithms/s3_fifo/s3Fifo.cpp.o.d"
+  "/home/tiasz/cache_algorithms/core/core.cpp" "CMakeFiles/cache_simulator.dir/core/core.cpp.o" "gcc" "CMakeFiles/cache_simulator.dir/core/core.cpp.o.d"
+  "/home/tiasz/cache_algorithms/core/disk_io/diskIo.cpp" "CMakeFiles/cache_simulator.dir/core/disk_io/diskIo.cpp.o" "gcc" "CMakeFiles/cache_simulator.dir/core/disk_io/diskIo.cpp.o.d"
+  "/home/tiasz/cache_algorithms/docs/docs.cpp" "CMakeFiles/cache_simulator.dir/docs/docs.cpp.o" "gcc" "CMakeFiles/cache_simulator.dir/docs/docs.cpp.o.d"
+  "/home/tiasz/cache_algorithms/ra2_main.cpp" "CMakeFiles/cache_simulator.dir/ra2_main.cpp.o" "gcc" "CMakeFiles/cache_simulator.dir/ra2_main.cpp.o.d"
+  "/home/tiasz/cache_algorithms/simulation/simulator.cpp" "CMakeFiles/cache_simulator.dir/simulation/simulator.cpp.o" "gcc" "CMakeFiles/cache_simulator.dir/simulation/simulator.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cache_simulator.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cache_simulator.
+# This may be replaced when dependencies are built.
