67f18b12669779da00e1a766bf7c4187805b9761
Zuniga Matias (CtP/ETS) <Matias.Zuniga@br.bosch.com>
2025-10-13 20:20:29 -0300

    get and put created

--- algorithms/lfu/lfu.cpp
+++ algorithms/lfu/lfu.cpp
@@ -6,6 +6,37 @@ Lfu::~Lfu() {
     clear();
 }
 
+std::optional<std::string> Lfu::get(const std::string& key) {
+    auto it = keyToNode.find(key);
+    if (it == keyToNode.end()) {
+        return std::nullopt;
+    }
+    
+    std::string value = it->second->value;
+    updateFrequency(key);
+    return value;
+}
+
+void Lfu::put(const std::string& key, const std::string& value) {
+    if (capacity <= 0) return;
+    
+    auto it = keyToNode.find(key);
+    if (it != keyToNode.end()) {
+        it->second->value = value;
+        updateFrequency(key);
+        return;
+    }
+    
+    if (currentSize >= capacity) {
+        evictLfu();
+    }
+    
+    frequencyGroups[1].emplace_front(key, value);
+    keyToNode[key] = frequencyGroups[1].begin();
+    minFrequency = 1;
+    currentSize++;
+}
+
 bool Lfu::contains(const std::string& key) const {
     return keyToNode.find(key) != keyToNode.end();
 }
