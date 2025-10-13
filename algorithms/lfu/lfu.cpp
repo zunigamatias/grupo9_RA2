3131fedda534e7d43b03c12f3eeb4ff6f345b142
Zuniga Matias (CtP/ETS) <Matias.Zuniga@br.bosch.com>
2025-10-13 20:20:06 -0300

    updateFrequency and evictLfu created

--- algorithms/lfu/lfu.cpp
+++ algorithms/lfu/lfu.cpp
@@ -16,3 +16,32 @@ void Lfu::clear() {
     currentSize = 0;
     minFrequency = 0;
 }
+
+void Lfu::updateFrequency(const std::string& key) {
+    auto nodeIt = keyToNode[key];
+    int oldFreq = nodeIt->frequency;
+    int newFreq = oldFreq + 1;
+    
+    LfuNode newNode = *nodeIt;
+    newNode.frequency = newFreq;
+    
+    frequencyGroups[oldFreq].erase(nodeIt);
+    
+    frequencyGroups[newFreq].emplace_front(newNode);
+    keyToNode[key] = frequencyGroups[newFreq].begin();
+    
+    if (frequencyGroups[minFrequency].empty()) {
+        minFrequency++;
+    }
+}
+
+void Lfu::evictLfu() {
+    if (frequencyGroups[minFrequency].empty()) return;
+    
+    auto& minFreqList = frequencyGroups[minFrequency];
+    std::string keyToRemove = minFreqList.back().key;
+    
+    minFreqList.pop_back();
+    keyToNode.erase(keyToRemove);
+    currentSize--;
+}
