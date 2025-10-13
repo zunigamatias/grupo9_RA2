4fcb67addf65c044402085e59a940911a49b62c8
Zuniga Matias (CtP/ETS) <Matias.Zuniga@br.bosch.com>
2025-10-13 20:50:22 -0300

    adjusted file path and formatting

--- ra2_main.cpp
+++ ra2_main.cpp
@@ -79,7 +79,7 @@ int main() {
             continue;
         }
 
-        std::string fileName = "text_" + std::to_string(choice) + ".txt";
+        std::string fileName = "../texts/text_" + std::to_string(choice) + ".txt";
 
         auto start = std::chrono::high_resolution_clock::now();
         bool hit = false;
@@ -105,10 +105,11 @@ int main() {
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> elapsed = end - start;
 
-        std::cout << "Text " << choice << " loaded in " << elapsed.count() << " ms.\n";
-
+        
         std::cout << "---- Full text ----\n";
-        std::cout << item.value() << "...\n";
+        std::cout << item.value() << std::endl;
+        std::cout << "---- End of text ----\n";
+        std::cout << "Text " << choice << " loaded in " << elapsed.count() << " ms.\n";
     }
 
     return 0;
